2dec0738ef3e06112a40c5baf2b940f0599fbe35
Martin Schmollinger <martin.schmollinger@gmail.com>
2018-09-10 15:44:48 +0200

    Added some style and removed unnecessary logging messages

--- src/main/java/rocks/turncodr/mycurriculum/controller/AboutController.java
+++ src/main/java/rocks/turncodr/mycurriculum/controller/AboutController.java
@@ -1,10 +1,7 @@
 package rocks.turncodr.mycurriculum.controller;
 
-import java.lang.invoke.MethodHandles;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,11 +18,10 @@ import rocks.turncodr.mycurriculum.services.DegreeProgramJpaRepository;
 public class AboutController {
     @Autowired
     private DegreeProgramJpaRepository degreeProgramService;
-    private final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     @GetMapping("/about")
     public String about(Model model) {
         List<DegreeProgram> degreePrograms = degreeProgramService.findAll();
-        logger.info(degreePrograms.toString());
         model.addAttribute("degreePrograms", degreePrograms);
         return "about";
     }
