b5bea65f3bafb64443fd334fa29d39bd5f10b2f8
Martin Schmollinger <martin.schmollinger@gmail.com>
2018-09-10 11:53:57 +0200

    Added checkstyle plugin and configuration. Correction of non-well-formed classes.

--- src/main/java/rocks/turncodr/mycurriculum/controller/AboutController.java
+++ src/main/java/rocks/turncodr/mycurriculum/controller/AboutController.java
@@ -3,11 +3,15 @@ package rocks.turncodr.mycurriculum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller class directing to the about page of the application.
+ *
+ */
 @Controller
 public class AboutController {
-	
-	@GetMapping("/about")
-	public String about() {
-		return "about";
-	}
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
 }
--- src/test/java/rocks/turncodr/mycurriculum/MyCurriculumApplicationTests.java
+++ src/test/java/rocks/turncodr/mycurriculum/MyCurriculumApplicationTests.java
@@ -5,12 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * This is just an auto-generated test class.
+ *
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyCurriculumApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
