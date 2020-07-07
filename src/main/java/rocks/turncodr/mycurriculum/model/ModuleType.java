0aacc7164b73779a8314e164ec6cbd9635562e2b
GermanRanger <k7-avenger@web.de>
2020-07-07 18:24:10 +0200

    #18 display WP modules in the module list
    edited exregSyllabus.html
         -refactoring tab spaces
         -added WP module list after ECTS
          sum in the table of contents
          -added WP module list with
            specific information to the end of
           the site
          - deleted specific information of
             the placeholder modules on the
             site
         -changed anchor point of the
           placeholder modules to the WP
           module list

--- src/main/java/rocks/turncodr/mycurriculum/model/ModuleType.java
+++ src/main/java/rocks/turncodr/mycurriculum/model/ModuleType.java
@@ -2,7 +2,7 @@ package rocks.turncodr.mycurriculum.model;
 
 public enum ModuleType {
     MAIN_MODULE(0),
-    PLACEHOLDER_MODULE(1);
+    PLACEHOLDER_MODULE(1),
     ELECTIVE_MODULE(2);
 
     private int index;
