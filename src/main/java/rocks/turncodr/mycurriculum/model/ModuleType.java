b7d8d43bce876e12e9856633a589ee648d0f642d
GermanRanger <k7-avenger@web.de>
2020-07-14 14:27:09 +0200

    Improvements after review
    - changed wildcard imports to single imports
    - simplified enum and replaced the getIndex() with ordinal() method

--- src/main/java/rocks/turncodr/mycurriculum/model/ModuleType.java
+++ src/main/java/rocks/turncodr/mycurriculum/model/ModuleType.java
@@ -4,17 +4,7 @@ package rocks.turncodr.mycurriculum.model;
  *  Attribute for Module.
  */
 public enum ModuleType {
-    MAIN_MODULE(0),
-    PLACEHOLDER_MODULE(1),
-    ELECTIVE_MODULE(2);
-
-    private int index;
-
-    ModuleType(int index) {
-        this.index = index;
-    }
-
-    public int getIndex() {
-        return index;
-    }
+    MAIN_MODULE,
+    PLACEHOLDER_MODULE,
+    ELECTIVE_MODULE;
 }
