35ed0b1685290a9a605906d93cc038fe92627a9c
TheDelta891 <tim.meinhof@gmail.com>
2020-07-19 11:20:59 +0200

    [MYC-24] added spring security config class with configurations for the login / pom.xml extended by spring security library / added login access denied handler for incorrect login access

---
+++ src/main/java/rocks/turncodr/mycurriculum/error/LoginAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package rocks.turncodr.mycurriculum.error;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class LoginAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginAccessDeniedHandler.class);
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            logger.info(auth.getName()
+                    + " was trying to access protected resource: "
+                    + httpServletRequest.getRequestURI());
+        }
+
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/access-denied");
+
+    }
+}
