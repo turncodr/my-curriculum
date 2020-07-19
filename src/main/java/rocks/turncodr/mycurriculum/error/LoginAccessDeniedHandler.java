77e005ffa5b86253f84fe5326bcbeab531336db9
TheDelta891 <tim.meinhof@gmail.com>
2020-07-19 13:13:04 +0200

    [MYC-24] Checkstyle adaption imports

--- src/main/java/rocks/turncodr/mycurriculum/error/LoginAccessDeniedHandler.java
+++ src/main/java/rocks/turncodr/mycurriculum/error/LoginAccessDeniedHandler.java
@@ -1,5 +1,8 @@
 package rocks.turncodr.mycurriculum.error;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
@@ -8,29 +11,25 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Handler for secured pages.
  */
 @Component
 public class LoginAccessDeniedHandler implements AccessDeniedHandler {
+  private final Logger log = LoggerFactory.getLogger(LoginAccessDeniedHandler.class);
 
-    private final Logger log = LoggerFactory.getLogger(LoginAccessDeniedHandler.class);
+  @Override
+  public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                     AccessDeniedException e) throws IOException {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-    @Override
-    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-//        if (auth != null) {
-//            log.info(auth.getName()
-//                    + " was trying to access protected resource: \n"
-//                    + httpServletRequest.getRequestURI() + "\n");
-//        }
-
-        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/access-denied");
-
+    if (auth != null) {
+      log.info(auth.getName()
+                    + " was trying to access protected resource: \n"
+                    + httpServletRequest.getRequestURI() + "\n");
     }
+    httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/access-denied");
+
+  }
 }
