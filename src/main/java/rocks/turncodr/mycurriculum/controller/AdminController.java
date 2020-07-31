eae60da1adbcbb30254876d3a53e3b8c4dfa15b9
Alexander Voigt <alexander.voigt@student.reutlingen-university.de>
2020-07-31 17:49:06 +0200

    [MYC-31] Create admin page

--- src/main/java/rocks/turncodr/mycurriculum/controller/AdminController.java
+++ src/main/java/rocks/turncodr/mycurriculum/controller/AdminController.java
@@ -1,7 +1,15 @@
 package rocks.turncodr.mycurriculum.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import rocks.turncodr.mycurriculum.model.Curriculum;
+import rocks.turncodr.mycurriculum.model.Module;
+import rocks.turncodr.mycurriculum.services.CurriculumJpaRepository;
+import rocks.turncodr.mycurriculum.services.ModuleJpaRepository;
+
+import java.util.List;
 
 /**
  * Controller class directing to the admin page.
@@ -9,8 +17,24 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class AdminController {
+
+    @Autowired
+    private CurriculumJpaRepository curriculumRepository;
+
+    @Autowired
+    private ModuleJpaRepository moduleRepository;
+
+    /**
+     * Loads all modules and curricula and adds them to the model. Then, it returns the index.html
+     * @param model the spring boot model
+     * @return the index.html for the admin page
+     */
     @GetMapping("/admin")
-    public String getAdminHome() {
+    public String getAdminHome(Model model) {
+        List<Curriculum> curricula = curriculumRepository.findAll();
+        List<Module> modules = moduleRepository.findAll();
+        model.addAttribute("moduleList", modules);
+        model.addAttribute("curricula", curricula);
         return "admin/index";
     }
 
